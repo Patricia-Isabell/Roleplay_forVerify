b54b98aed2bfd6f4f473e850757d835a93d192e8
Luca224lu90 <ammirati1990@gmail.com>
2025-08-31 18:08:21 +0200

    fehler fixing

--- unser-react-projekt/src/data/Ereigniskarten/C1g.js
+++ unser-react-projekt/src/data/Ereigniskarten/C1g.js
@@ -2,11 +2,12 @@
   {
     "id": "C1g",
     "question": "Welche Information ist aus dem Periodensystemder Elemente ableitbar? (Mehrfachnennungen möglich)",
-    "answers":
-    "a) Das Gewicht eines Stoffes
-b) Chemische Eigenschaften wie die Neigung,Rost anzusetzen
-c) Ob es sich bei Raumtemperatur um ein Gas oder einen festen Stoff handelt
-d) Welche Farbe der Stoff besitzt",
+    "answers": [
+      "a) Das Gewicht eines Stoffes",
+      "b) Chemische Eigenschaften wie die Neigung,Rost anzusetzen",
+      "c) Ob es sich bei Raumtemperatur um ein Gas oder einen festen Stoff handelt",
+      "d) Welche Farbe der Stoff besitzt"
+    ],
     "correct": "Das Gewicht eines Stoffes/ Chemische Eigenschaften wie die Neigung,Rost anzusetzen",
     "explanation": "In seiner einfachsten Ausführung gibt das Periodensystem neben Nummer und Namenskürzel des Elements nur die Atomkernmasse an. Oft bietet die Übersichtstafel aber noch mehr Informationen wie etwa den Aggregatzustand, der besagt, ob ein Stoff bei Raumtemperatur fest, flüssig oder gasförmig ist, oder die sogenannten Oxidationsstufen, die ein Element annehmen kann, indem es Elektronen aufnimmt oder abgibt. Daraus kann der Chemiker entnehmen, wie sich Stoffe vermutlich verhalten und welche Verbindungen sie eingehen können. Die Farbe von Stoffen lässt sich nicht vorhersagen."
   }
