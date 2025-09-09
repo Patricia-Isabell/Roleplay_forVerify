1211e9fe709ac5233e99effb43f0cce3b03dfe6e
Patricia-Isabell <patricia.wiedemann@gmx.de>
2025-09-09 10:18:03 +0200

    Hintergrundbild Fragen Wpfw

--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-/* export { default as Data } from "./Data";
-export { default as Final } from "./Final";
-export { default as Game } from "./Game";
-export { default as Lib } from "./Lib";
-export { default as Status } from "./Status";
-export { default as Util } from "./Util";
- */
